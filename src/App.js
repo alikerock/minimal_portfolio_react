3fcbfae1b0c0427d99bd25dcf20b64618e5d47bd
rock's ezweb <alikerock@gmail.com>
2024-12-05 16:06:09 +0900

    router add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PortfolioList from "./components/PortfolioList";
+import PortfolioSingle from "./components/PortfolioSingle";
+import data from "./data.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<PortfolioList data={data} />} />
+        <Route path="/portfolio/:id" element={<PortfolioSingle data={data} />} />
+      </Routes>
+    </Router>
   );
 }
 
